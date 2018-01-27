b98e394270ca397c7acb6ae11111601a56b6fee7
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-01-26 20:28:13 -0800

    added inquirer prompts

--- liri.js
+++ liri.js
@@ -42,7 +42,7 @@ if (command === 'do-what-it-says') {
 //------------------------------------Launch Twitter 
 function getTweets() {
 
-  console.log()
+  console.log('GET TWEETS!')
 
 	var client = new Twitter({
  		consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -51,12 +51,25 @@ function getTweets() {
  		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 	});
 
-	var params = {screen_name: 'NoThanksTweeter',
-			  count: '20'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  		if (error) {
-  		console.log(error);
-  	}
+   inquirer.prompt([
+  {
+    name: 'username',
+    message: 'Enter the Twitter Username of the Tweets to be Searched: '
+  }, {
+    name: 'numTweets',
+    message: 'How Many Tweets Would You Like to Read? : '
+  }
+
+  ]).then(function(answers) {
+    var answersObj = answers
+    var usernameAnswer = answers.username;
+    var countAnswer = answers.numTweets;
+	  var params = {screen_name: usernameAnswer,
+			  count: countAnswer};
+	      client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  		    if (error) {
+  		      console.log(error);
+  	      }
 
   	for (var i = 0; i < tweets.length; i++) {
   		//console logging the tweets
@@ -65,7 +78,10 @@ function getTweets() {
   		console.log('Created at ' + tweets[i].created_at);
   	}
 	
-	});
+	 });
+  
+  });
+
 
 }
 
@@ -77,59 +93,77 @@ function getSpotify() {
   		secret: process.env.SPOTIFY_SECRET
 	});
 
-	//--------------------------Set Default song to "The Sign"
-	if (songName === undefined) {
- 		 songName = "The Sign"; 
- 	}
 
-	spotify.search({ type: 'track', query: songName, limit: '1' }, function(err, data) {
+  inquirer.prompt([
+  {
+    name: 'songName',
+    message: 'What Song would you like to Search? : '
+  }
+  ]).then(function(answers) {
+    var songAnswer = answers.songName;
+      //--------------------------Set Default song to "The Sign"
+    if (songAnswer === '') {
+      songAnswer = "The Sign by Ace of Base"; 
+    }
+
+	  spotify.search({ type: 'track', query: songAnswer }, function(err, data) {
   		if (err) {
     	return console.log('Error occurred: ' + err);
  		} 
- 		console.log(songName);
- 		
+
  		//console logging the artist name
  		console.log('Artist Name: ' + data.tracks.items[0].album.artists[0].name);
  		//console logging the song name
  		console.log('Song Name: ' + data.tracks.items[0].name);
  		//console logging the link to the song
- 		console.log('Link to Song: ' + data.tracks.items[0].album.artists[0].href);
+ 		console.log('Link to Song: ' + data.tracks.items[0].album.external_urls.spotify);
  		//console logging the album name
  		console.log('Album Name: ' + data.tracks.items[0].album.name);
 
 
-	});	
+	  });	
+
+
+  });
+
 	
 }
 
 //------------------------------Launch OMDB using Request
 function getMovie() {
 
+  inquirer.prompt([
+  {
+    name: 'movieName',
+    message: 'What Movie would you like to Search? :'
+  }
+  ]).then(function(answers) {
+     var movieAnswer = answers.movieName;
 	//--------------------------Set Default Movie to "Mr Nobody"
-	if (movieTitle === undefined) {
-			movieTitle = 'Mr+Nobody';
-	}
+	    if (movieAnswer === '') {
+			   movieAnswer = 'Mr+Nobody';
+	    }
 
-	request('http://www.omdbapi.com/?apikey=c628a260&t=' + movieTitle, function (error, response) {
-  	var obj = JSON.parse(response.body);
-  	//console logging the Movie Title
-  		console.log('Movie Title: ' + obj.Title);
-  	//console logging the release Year
-  		console.log('Release Year: ' + obj.Year);
-  	//console logging the IMDB rating
-  		console.log('IMDB Rating: ' + obj.Ratings[0].Value);
-  	//console logging the RT rating
-  		console.log('Rotten Tomatoes Rating: ' + obj.Ratings[1].Value);
-  	//console logging country of production
-  		console.log('Produced in: ' + obj.Country);
-  	//console logging the movie language(s)
-  		console.log('Movie Language: ' + obj.Language);
-  	//console logging the plot of the movie
-  		console.log('Plot: ' + obj.Plot);
-  	//console logging the actors in the movie
-  		console.log('Actors: ' + obj.Actors)
-	});
-
+	   request('http://www.omdbapi.com/?apikey=c628a260&t=' + movieAnswer, function (error, response) {
+  	   var obj = JSON.parse(response.body);
+  	   //console logging the Movie Title
+  		    console.log('Movie Title: ' + obj.Title);
+  	   //console logging the release Year
+  		    console.log('Release Year: ' + obj.Year);
+  	   //console logging the IMDB rating
+  		    console.log('IMDB Rating: ' + obj.Ratings[0].Value);
+  	   //console logging the RT rating
+  		    console.log('Rotten Tomatoes Rating: ' + obj.Ratings[1].Value);
+  	   //console logging country of production
+  		    console.log('Produced in: ' + obj.Country);
+  	   //console logging the movie language(s)
+  		    console.log('Movie Language: ' + obj.Language);
+  	   //console logging the plot of the movie
+  		    console.log('Plot: ' + obj.Plot);
+  	   //console logging the actors in the movie
+  		    console.log('Actors: ' + obj.Actors)
+	   });
+  });
 
 }
 
@@ -165,37 +199,6 @@ function searchTxtFiles() {
   });
 
 }
-
-// function twitterInquirer() {
-
-//   inquirer.prompt([
-//   {
-//     name: "name",
-//     message: "What is your name?"
-//   }, {
-//     name: "position",
-//     message: "What is your current position?"
-//   }, {
-//     name: "age",
-//     message: "How old are you?"
-//   }, {
-//     name: "language",
-//     message: "What is your favorite programming language?"
-//   }
-// ]).then(function(answers) {
-//   // initializes the variable newguy to be a programmer object which will take
-//   // in all of the user's answers to the questions above
-//   var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
-//   // printInfo method is run to show that the newguy object was successfully created and filled
-//   newGuy.printInfo();
-// });
-
-
-
-
-
-// }
-
 
 
 
